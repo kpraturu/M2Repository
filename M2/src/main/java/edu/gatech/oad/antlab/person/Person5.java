b1ccdebaf3e9f3e45baec565579fe54120f360b3
Aaron <aaronstrickland81@gmail.com>
2016-09-06 18:54:48 -0400

    Edited a syntax error and added an edge case of length 1

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -30,10 +30,13 @@ public class Person5 {
    * @return the modified string
    */
   private String calc(String input) {
+    if (input.length() == 1) {
+        return input;
+    }
     char first = input.charAt(0);
     char second = input.charAt(1);
     String toReturn = "";
-    for (int i = 2, i < input.length(), i++) {
+    for (int i = 2; i < input.length(); i++) {
       toReturn += input.charAt(i);
     }
     toReturn = toReturn + first + second;
